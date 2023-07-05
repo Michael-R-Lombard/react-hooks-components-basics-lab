d69f2bd0b13b7d742b58dfdd4e2e8df9ff2d7234
Michael Lombard <miklombard@yahoo.com>
2023-07-05 12:15:44 -0400

    updated lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,12 @@
 import React from "react";
+import About from "./About";
+import NavBar from "./NavBar";
+import Home from "./Home";
 
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
 
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
 
-function About() {
-  return(
-    <div id="about">
-      <h2>About</h2>
-    </div>
-  )
-}
+
+
 
 
 function App() {
